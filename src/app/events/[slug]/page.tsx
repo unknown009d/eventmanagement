a1ff067e57dc88b2bcecbb0d1b3cb6245852e824
unknown009d <vbunitynet@gmai.com>
2024-04-01 12:11:51 +0530

    added design

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -12,13 +12,17 @@ export default function Page({ params }: { params: { slug: string } }) {
     return (
       <div>
         <Image src={`/EventImages/${pageEvent.img}`} alt={pageEvent.title} width={800} height={600} className="w-full object-cover" />
-        <h2>{pageEvent.title}</h2>
-        <p>{pageEvent.details}</p>
-        <p>{pageEvent.location}</p>
-        <p>{pageEvent.date}</p>
+        <div className="mt-4 flex flex-col gap-2 px-6">
+          <h2 className="font-bold text-2xl">{pageEvent.title}</h2>
+          <div className="flex justify-between">
+            <p>{pageEvent.location}</p>
+            <p>{pageEvent.date}</p>
+          </div>
+          <p className="opacity-80 text-left mt-2">{pageEvent.details}</p>
+        </div>
       </div>
     )
   }
 
-  return <p className="text-lg opacity-80">Event not found</p>
+  return <p className="text-lg opacity-60">Event not found</p>
 }
